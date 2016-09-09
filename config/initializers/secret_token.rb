30965e73b126fdc3ec0f89c5c493d0fa2f9563ff
hanhttm96 <myhanhqt96@gmail.com>
2016-09-09 10:14:23 +0700

    9/9

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hanhttm::Application.config.secret_key_base = '548aa954eee9ff32ff2dfe45690dd61c48035b5d6de028e9866e7c5585883f27bea7e0254425ba4bc2c235719e0af0d199c317b5a2df79aaca915a0ce8fecbbd'
